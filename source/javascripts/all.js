8b3ec67fd443b3450a3509ebd149105c23702d12
Laura Cabrera <lzcabrera@gmail.com>
2015-02-14 20:57:18 -0800

    enhance sliding menu interaction

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,12 +6,14 @@
 $(document).ready(function () {
   $('.js-menu-trigger').on('click touchstart', function (e) {
     $('.js-menu').toggleClass('is-visible');
+    $('.js-main-push').toggleClass('menu-is-visible');
     $('.js-menu-screen').toggleClass('is-visible');
     e.preventDefault();
   });
 
   $('.js-menu-screen').on('click touchstart', function (e) {
     $('.js-menu').toggleClass('is-visible');
+    $('.js-main-push').toggleClass('menu-is-visible');
     $('.js-menu-screen').toggleClass('is-visible');
     e.preventDefault();
   });
