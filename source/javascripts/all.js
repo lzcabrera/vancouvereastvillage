12ceb022dd31ba0d83414c9d3666b3abc2e243c7
Laura Cabrera <lzcabrera@gmail.com>
2015-02-18 19:43:30 -0800

    add opacity to menu screen and add width:100% when using menu toggle

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -19,8 +19,9 @@ $(document).ready(function () {
     $('.js-menu').toggleClass('is-visible');
     $('.js-main-push').toggleClass('menu-is-visible');
     $('.js-menu-screen').toggleClass('is-visible');
+
     if(!$('.js-main-push').hasClass('menu-is-visible')){
-      $('.js-main-push').width($('.js-main-push').width()+220);
+      $('.js-main-push').width('100%');
     }
     e.preventDefault();
   });
